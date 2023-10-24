b320fcde61dc4b1f4ff23acca879e9136ce7e49d
KOCWIN <147073954+KOCW1N@users.noreply.github.com>
2023-10-24 21:34:19 +0200

    Update com.js

--- src/pages/home/com.js
+++ src/pages/home/com.js
@@ -3,7 +3,7 @@ import { GetCom, deletePost} from '../../firebase';
 import React, { useEffect, useState, useContext, createContext } from 'react';
 
 function Com(){
-//testdddddjjjj
+//testdddddjjjjhh
 }
   //test  
 function DeleteButton( ComId ) {
