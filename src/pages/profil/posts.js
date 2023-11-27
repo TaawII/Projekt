c6150151c7c93a1bbc1dc53c298b7973e08104f0
KOCWIN <147073954+KOCW1N@users.noreply.github.com>
2023-11-27 01:00:25 +0100

    dokonczenie retweeta
    dokonczenie retweeta

--- src/pages/profil/posts.js
+++ src/pages/profil/posts.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { getFirestore, collection, query, where, getDocs, orderBy } from 'firebase/firestore';
+import { getFirestore, collection, query, where, getDocs, orderBy, doc, getDoc } from 'firebase/firestore';
 import { getStorage, ref, getDownloadURL } from 'firebase/storage'; 
 import { formatDistanceToNow } from 'date-fns';
 import { pl } from 'date-fns/locale';
-import { Avatar } from "@mui/material"
+import { Avatar } from "@mui/material";
 
 function UserPostsFetcher() {
   const [userPosts, setUserPosts] = useState([]);
@@ -18,30 +18,76 @@ function UserPostsFetcher() {
 
     const fetchUserPosts = async () => {
       const db = getFirestore();
-      const postsCollection = collection(db, 'wpisy');
-      const q = query(postsCollection, where('UID', '==', uid), orderBy('Timestamp', 'desc'));
+
+      const userPostsCollection = collection(db, 'wpisy');
+      const userPostsQuery = query(userPostsCollection, where('UID', '==', uid), orderBy('Timestamp', 'desc'));
+
+      const retweetsCollection = collection(db, 'retweets');
+      const retweetsQuery = query(retweetsCollection, where('UserID', '==', uid), orderBy('Timestamp', 'desc'));
+
+      const storage = getStorage();
 
       try {
-        const querySnapshot = await getDocs(q);
-        const posts = querySnapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
+        const userPostsSnapshot = await getDocs(userPostsQuery);
+        const userPosts = userPostsSnapshot.docs.map(async (doc) => {
+          const post = {
+            id: doc.id,
+            ...doc.data(),
+          };
 
-        for (const post of posts) {
-          const storage = getStorage();
           const avatarRef = ref(storage, `${post.UID}.png`);
 
-          getDownloadURL(avatarRef)
-            .then((url) => {
-              setUserPhotoURL(url);
-            })
-            .catch((error) => {
-              console.error('Błąd podczas pobierania avatara z Firebase Storage:', error);
-            });
-        }
+          try {
+            const url = await getDownloadURL(avatarRef);
+            post.userPhotoURL = url;
+          } catch (error) {
+            console.error('Błąd podczas pobierania avatara z Firebase Storage:', error);
+          }
 
-        setUserPosts(posts);
+          return post;
+        });
+
+        const retweetsSnapshot = await getDocs(retweetsQuery);
+        const retweets = retweetsSnapshot.docs.map(async (retweetDoc) => {
+          const retweet = {
+            id: retweetDoc.id,
+            ...retweetDoc.data(),
+          };
+
+          const retweetedPostRef = doc(db, 'wpisy', retweet.PostId);
+          const retweetedPostDoc = await getDoc(retweetedPostRef);
+          
+          if (retweetedPostDoc.exists()) {
+            const retweetedPost = {
+              id: retweetedPostDoc.id,
+              ...retweetedPostDoc.data(),
+            };
+
+            const avatarRef = ref(storage, `${retweetedPost.UID}.png`);
+
+            try {
+              const url = await getDownloadURL(avatarRef);
+              retweet.userPhotoURL = url;
+            } catch (error) {
+              console.error('Błąd podczas pobierania avatara z Firebase Storage:', error);
+            }
+
+            return {
+              ...retweet,
+              ...retweetedPost,
+            };
+          } else {
+            return null;
+          }
+        });
+
+        const userPostsWithAvatars = await Promise.all(userPosts);
+        const retweetsWithAvatars = (await Promise.all(retweets)).filter(Boolean); 
+
+        const combinedPosts = [...userPostsWithAvatars, ...retweetsWithAvatars];
+        const sortedPosts = combinedPosts.sort((a, b) => b.Timestamp.toDate() - a.Timestamp.toDate());
+
+        setUserPosts(sortedPosts);
       } catch (error) {
         console.error('Błąd podczas pobierania postów:', error);
       }
@@ -57,7 +103,7 @@ function UserPostsFetcher() {
         {userPosts.map((post) => (
           <li key={post.id} className="post">
             <div className="userPost">
-              <Avatar alt={post.Pseudonim} src={userPhotoURL} />
+              <Avatar alt={post.Pseudonim} src={post.userPhotoURL} />
               <div>
                 <strong>{post.Pseudonim}</strong>
                 <div className="postTime">
